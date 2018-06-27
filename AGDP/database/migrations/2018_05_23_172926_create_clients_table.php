c80c71547e224b38b35f11fd8f0d32d8f0f65add
Esperanza <maesbepi12@gmail.com>
2018-06-27 17:40:16 -0500

    Problem Mail 535

--- AGDP/database/migrations/2018_05_23_172926_create_clients_table.php
+++ AGDP/database/migrations/2018_05_23_172926_create_clients_table.php
@@ -16,11 +16,11 @@ class CreateClientsTable extends Migration {
 		Schema::create('clients', function(Blueprint $table)
 		{
 			$table->integer('idClient', true);
-			$table->string('nitClient', 50);
-			$table->string('nameClient', 50);
+			$table->string('nitClient', 50)->unique();
+			$table->string('nameClient', 50)->unique();
 			$table->string('personClient', 50);
 			$table->string('addressClient', 50)->unique();
-			$table->boolean('typeClient')->unique();
+			$table->boolean('typeClient');
 			$table->rememberToken();
             $table->timestamps();
 		});
