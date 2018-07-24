71448090a3411a59b4cee18dc42e0deef6f71c17
Esperanza <maesbepi12@gmail.com>
2018-07-24 17:41:08 -0500

    Frontend-->80%

--- AGDP/resources/views/Folder/lista.blade.php
+++ AGDP/resources/views/Folder/lista.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 
 @section('main')
+
 <div class="right_col" role="main">
 	<div class="clearfix"></div>
 	<div class="row">
@@ -17,43 +18,40 @@
 				</div>
 				<div class="x_content">
 					
-					<div class="col-md-12 col-sm-12 col-xs-12">
-						
-							
-							<div class="x_content">
-								<table id="datatable-responsive" class="table  table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
-									<thead>
-										<tr>
-											<th>{{$folder->}} </th>
-											<th>Cliente</th>
-											<th>Opciones</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td>1049638457</td>
-											<td>Cliente 1</td>
-											
-											<td>
-												<div class="dropdown">
-													<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-														<i class="fa fa-ellipsis-v"></i>
-													</button>
-													<div class="dropdown-menu" aria-labelledby="dropdownMenu1">
-													<li><a href="ver_proyecto.html">Ver</a></li>
-													<li><a href="editar_proyecto.html">Modificar</a></li>
-													<li class="lock"><a href="#">Desctivar</a></li>
-													<li class="hide unlock"><a href="#">Activar</a></li>
-													</div>
-													
-													</div>
+					<div class="col-md-12 col-sm-12 col-xs-12">							
+						<div class="x_content">
+							<table id="datatable-responsive" class="table  table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+								<thead>
+									<tr>
+										<th>Proyecto </th>
+										<th>Cliente</th>
+										<th>Opciones</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>1049638457</td>
+										<td>Cliente 1</td>
+										
+										<td>
+											<div class="dropdown">
+												<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+													<i class="fa fa-ellipsis-v"></i>
+												</button>
+												<div class="dropdown-menu" aria-labelledby="dropdownMenu1">
+												<li><a href="ver_proyecto.html">Ver</a></li>
+												<li><a href="editar_proyecto.html">Modificar</a></li>
+												<li class="lock"><a href="#">Desctivar</a></li>
+												<li class="hide unlock"><a href="#">Activar</a></li>
 												</div>
-											</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						
+												
+												</div>
+											</div>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
 					</div>
 				</div>
 			</div>
--- AGDP/resources/views/Layouts/main.blade.php
+++ AGDP/resources/views/Layouts/main.blade.php
@@ -5,99 +5,82 @@
 <div class="top_nav">
 <div class="container body">
     <div class="main_container">
-        <!-- <div class=" main-nav ">
-        <ul class="nav main-nav ">
-        <li class="nav-item">
-            <a class="nav-link active text-uppercase" href="#">Usuarios</a>
-        </li>
-        
-        <li class="nav-item">
-            <a class="nav-link text-uppercase" href="#">Areas de Trabajo</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-uppercase" href="#">Proyectos</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-uppercase" href="#">Clientes</a>
-        </li>
-        </ul>
-        </div> -->
-<div class="col-md-3 left_col">
-    <div class="left_col scroll-view">
-        <div class="navbar nav_title" style="border: 0;">
-            <a href="dashboard.html" class="site_title"><img src="{{('design/img/main/logo_1.png')}}"> <span>Consultecnicos</span></a>
-        </div>
+    <div class="col-md-3 left_col">
+        <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+                <a href="dashboard.html" class="site_title"><img src="{{('design/img/main/logo_1.png')}}"> <span>Consultecnicos</span></a>
+            </div>
 
-        <div class="clearfix"></div>
+            <div class="clearfix"></div>
 
-        <br><br>
+            <br><br>
 
-        <!-- sidebar menu -->
-        <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-            <div class="menu_section">
-                   <ul class="nav side-menu">
-                       <li><a><i class="fa fa-envelope-o"></i> Correspondencia <span class="fa fa-chevron-down"></span></a>
-                           <ul class="nav child_menu">
-                               <li><a>C. Enviada <span class="fa fa-chevron-down"></span></a>
-                                   <ul class="nav child_menu">
-                                       <li><a href="{{route('listaM')}}">Registrar Nueva</a></li>
-                                       <li><a href="{{('listaM')}}">Ver Todas</a></li>
-                                       <li><a href="#">Archivos adjuntos</a></li>
-                                   </ul>
-                               </li>
-                               <li><a>C. Recibida <span class="fa fa-chevron-down"></span></a>
-                                   <ul class="nav child_menu">
-                                       <li><a href="{{route('listaM')}}">Registrar Nueva</a></li>
-                                       <li><a href="{{route('listaM')}}">Ver Todas</a></li>
-                                       <li><a href="#">Archivos adjuntos</a></li>
-                                   </ul>
-                               </li>
-                           </ul>
-                       </li>
-                       <li><a><i class="fa fa-edit"></i> Proyectos <span class="fa fa-chevron-down"></span></a>
-                           <ul class="nav child_menu">
-                               <li><a href="{{route('nuevo')}}">Registrar Nuevo</a></li>
-                               <li><a href="{{route('lista')}}">Ver Todos</a></li>
-                           </ul>
-                       </li>
-                       <li><a><i class="fa fa-globe"></i> Clientes <span class="fa fa-chevron-down"></span></a>
-                           <ul class="nav child_menu">
-                               <li><a href="{{route('nuevoCl')}}">Registrar Nuevo</a></li>
-                               <li><a href="{{route('listaCl')}}">Ver Todos</a></li>
-                           </ul>
-                       </li>
-                       <li><a><i class="fa fa-users"></i> Usuarios <span class="fa fa-chevron-down"></span></a>
-                           <ul class="nav child_menu">
-                               <li><a href="{{route('newU')}}">Registrar Nuevo</a></li>
-                               <li><a href="{{route('listaU')}}">Ver Todos</a></li>
-                           </ul>
-                       </li>
-                       <li><a><i class="fa fa-bar-chart-o"></i> Informes <span class="fa fa-chevron-down"></span></a>
-                           <ul class="nav child_menu">
-                               <li><a href="#">C. Enviada</a></li>
-                               <li><a href="#">C. Recibida</a></li>
-                           </ul>
-                       </li>
-                   </ul>
-               </div>
-        
-
-        </div>
-        <!-- /sidebar menu -->
-
-        <!-- /menu footer buttons -->
-        <div class="sidebar-footer hidden-small">
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                <div class="menu_section">
+                       <ul class="nav side-menu">
+                           <li><a><i class="fa fa-envelope-o"></i> Correspondencia <span class="fa fa-chevron-down"></span></a>
+                               <ul class="nav child_menu">
+                                   <li><a>C. Enviada <span class="fa fa-chevron-down"></span></a>
+                                       <ul class="nav child_menu">
+                                           <li><a href="{{route('listaM')}}">Registrar Nueva</a></li>
+                                           <li><a href="{{('listaM')}}">Ver Todas</a></li>
+                                           <li><a href="#">Archivos adjuntos</a></li>
+                                       </ul>
+                                   </li>
+                                   <li><a>C. Recibida <span class="fa fa-chevron-down"></span></a>
+                                       <ul class="nav child_menu">
+                                           <li><a href="{{route('listaM')}}">Registrar Nueva</a></li>
+                                           <li><a href="{{route('listaM')}}">Ver Todas</a></li>
+                                           <li><a href="#">Archivos adjuntos</a></li>
+                                       </ul>
+                                   </li>
+                               </ul>
+                           </li>
+                           <li><a><i class="fa fa-edit"></i> Proyectos <span class="fa fa-chevron-down"></span></a>
+                               <ul class="nav child_menu">
+                                   <li><a href="{{route('nuevo')}}">Registrar Nuevo</a></li>
+                                   <li><a href="{{route('lista')}}">Ver Todos</a></li>
+                               </ul>
+                           </li>
+                           <li><a><i class="fa fa-globe"></i> Clientes <span class="fa fa-chevron-down"></span></a>
+                               <ul class="nav child_menu">
+                                   <li><a href="{{route('nuevoCl')}}">Registrar Nuevo</a></li>
+                                   <li><a href="{{route('listaCl')}}">Ver Todos</a></li>
+                               </ul>
+                           </li>
+                           <li><a><i class="fa fa-users"></i> Usuarios <span class="fa fa-chevron-down"></span></a>
+                               <ul class="nav child_menu">
+                                   <li><a href="{{route('newU')}}">Registrar Nuevo</a></li>
+                                   <li><a href="{{route('listaU')}}">Ver Todos</a></li>
+                               </ul>
+                           </li>
+                           <li><a><i class="fa fa-bar-chart-o"></i> Informes <span class="fa fa-chevron-down"></span></a>
+                               <ul class="nav child_menu">
+                                   <li><a href="#">C. Enviada</a></li>
+                                   <li><a href="#">C. Recibida</a></li>
+                               </ul>
+                           </li>
+                       </ul>
+                   </div>
             
-            <a data-toggle="tooltip" data-placement="top" title="Logout" href="iniciar_sesion.html">
-                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-            </a>
-            <a data-toggle="tooltip" data-placement="top" title="Logout" href="#">
-                <span class="glyphicon glyphicon glyphicon-question-sign" aria-hidden="true"></span>
-            </a>
+
+            </div>
+            <!-- /sidebar menu -->
+
+            <!-- /menu footer buttons -->
+            <div class="sidebar-footer hidden-small">
+                
+                <a data-toggle="tooltip" data-placement="top" title="Logout" href="iniciar_sesion.html">
+                    <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+                </a>
+                <a data-toggle="tooltip" data-placement="top" title="Logout" href="#">
+                    <span class="glyphicon glyphicon glyphicon-question-sign" aria-hidden="true"></span>
+                </a>
+            </div>
+            <!-- /menu footer buttons -->
         </div>
-        <!-- /menu footer buttons -->
     </div>
-</div>
     <div class="nav_menu">
         <nav>
             <div class="nav toggle">
